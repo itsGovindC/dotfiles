029247d300902f9c59b26174b3bde9e8e80cd351
Govind Chandak <govindchandak06@gmail.com>
2022-05-22 15:07:15 -0400

    url fixes

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt install tmux
 sudo apt install zsh
-sh -c "$(curl -fsSL https:\\raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
