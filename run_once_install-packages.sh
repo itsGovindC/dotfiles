1b236b7206417f168e9cc5289c38b7af70a9b61a
Govind Chandak <govindchandak06@gmail.com>
2022-05-22 15:23:34 -0400

    Removed install stuff

--- run_once_install-packages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install tmux
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
